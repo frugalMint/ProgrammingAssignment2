ecc6d156df3261aaa581214d158288883ea54c94
SATYAJIT CHATTERJEE <satyachatterjee007@gmail.com>
2017-07-12 15:28:56 +0530

    Implementation of makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implementation of caching to speed up our calculation
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix", which is really a list containing a function to:
+## set the value of the matrix, get the value of the matrix, set the value of the inverse, get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
